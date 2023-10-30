ee43918cbdf275cb804025c078e9ccb91fbebd17
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-10-30 15:54:40 -0600

    03 Estructura de carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  // <h1>Angular</h1>
+  // `,
   styleUrls: ['./app.component.css']
+  // styles: [`
+  // h1 {
+  //   color: green;
+  // }`]
 })
 export class AppComponent {
   title = 'angular_tutorial';
