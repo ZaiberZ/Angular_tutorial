14f57ea31861b4db310fd1055b05128d8cd2effb
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-11-01 20:06:10 -0600

    09 - Variables de plantilla (template variables)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, BotonesComponent],
+  declarations: [AppComponent, ContadorComponent, BotonesComponent, FormularioComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+  constructor() { }
+  ngOnInit(): void {
+
+  }
+
+  showConsole(name: string) {
+    console.log(name)
+  }
+
+}
