8929e2e8f84b43a1824e15f9eafd6d2a7a9f3662
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-11-01 19:57:41 -0600

    08 - Property Binding o enlaces de propiedades

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -11,6 +11,13 @@ export class BotonesComponent {
 
   }
 
-  text_color: string = '';
-  btn_disabled: boolean = false;
+  // text_color: string = '';
+  // btn_disabled: boolean = false;
+  
+  pexels: string[] = [
+    'https://images.pexels.com/photos/1906713/pexels-photo-1906713.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+    'https://images.pexels.com/photos/13866923/pexels-photo-13866923.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/3158814/pexels-photo-3158814.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  ];
+  src: string = this.pexels[0];
 }
