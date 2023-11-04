bbd8affbfb7f5601c243ff20e298f26d2adcd53a
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-11-04 16:03:29 -0600

    11 - Directiva ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { BuclesComponent } from './bucles/bucles.component';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, BotonesComponent, FormularioComponent],
+  declarations: [AppComponent, ContadorComponent, BotonesComponent, FormularioComponent, BuclesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
