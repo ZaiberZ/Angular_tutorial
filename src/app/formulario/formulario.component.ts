bdb931931f4d7e91890d131200936b430822a1ed
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-11-04 15:51:37 -0600

    10 - Directiva ngIf / Else

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
+  openAlert: boolean = false;
+
   constructor() { }
   ngOnInit(): void {
 
   }
 
+
   showConsole(name: string) {
-    console.log(name)
+    console.log(name);
+    this.openAlert = true;
   }
 
 }
