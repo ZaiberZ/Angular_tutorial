0b6b04de973acea9ca34a9e434066a67a8571f8b
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-11-05 13:22:39 -0600

    12 - Directiva Switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { BuclesComponent } from './bucles/bucles.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, BotonesComponent, FormularioComponent, BuclesComponent],
+  declarations: [AppComponent, ContadorComponent, BotonesComponent, FormularioComponent, BuclesComponent, SwitchComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/switch/switch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch',
+  templateUrl: './switch.component.html',
+  styleUrls: ['./switch.component.css']
+})
+export class SwitchComponent {
+  dia: number = 9;
+  cad: string = "Ab";
+  nval?: string;
+}
